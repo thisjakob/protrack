c458499a6277ed1bce06049c0863e9af471ab61b
gip <pascal@GipAir.local>
2015-10-06 07:36:49 +0200

    change ng-router to ui-router

--- app/scripts/protrack.js
+++ app/scripts/protrack.js
@@ -1,14 +1,17 @@
 'use strict';
 
-angular.module('protrack', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'firebase', 'ngRoute'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-;
+angular.module('protrack', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'firebase', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+          .state('home', {
+            url: '/',
+            templateUrl: 'partials/main.html',
+              controller: 'MainCtrl'
+          })
+          .state('about', {
+            url: '/',
+            templateUrl: 'partials/about.html'
+          });
+        $urlRouterProvider.otherwise('/');
+  });
