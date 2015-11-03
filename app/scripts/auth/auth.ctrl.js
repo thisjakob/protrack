13a90c0132f7f367fde890c7d7d458baadcfebd0
thisjakob <this@mathias-jakobs-macbook.local>
2015-11-03 23:41:42 +0100

    merged auth.ctrl.js and auth.service .js

--- app/scripts/auth/auth.ctrl.js
+++ app/scripts/auth/auth.ctrl.js
@@ -1,62 +1,72 @@
-(function () {
-    'use strict';
-    angular.module('protrack')
-        .controller('AuthCtrl', ['Auth', '$state', function (Auth, $state) {
-            var authCtrl = this;
+'use strict';
 
-            authCtrl.message = 'Message: ';
-            authCtrl.auth = Auth;
+angular.module('protrack')
+    .controller('AuthCtrl', ['Auth','$state', 'dataService', function(Auth, $state, dataService){
+        var authCtrl = this;
 
-            // any time auth status updates, add the user data to scope
-            authCtrl.auth.$onAuth(function (authData) {
+        authCtrl.authData = false;
+        authCtrl.message = null;
+        authCtrl.auth = Auth;
+
+        // any time auth status updates, add the user data to scope
+        authCtrl.auth.$onAuth(function(authData) {
+            if ( authData ) {
                 authCtrl.authData = authData;
+                dataService.getDataObj('users/' + authData.uid + '/firstname').$loaded(function(data){
+                    authCtrl.firstname = data.$value;
+                });
+            }
+        });
+
+        authCtrl.login = function(){
+            authCtrl.auth.$authWithPassword({
+                email:authCtrl.email,
+                password:authCtrl.password
+            })
+            .then(function(authData){
+                authCtrl.email = '';
+                authCtrl.password = '';
+                authCtrl.message = 'You are logged in as ' + authData.uid;
+            })
+            .catch(function(error){
+                authCtrl.error = error;
             });
+        };
 
+        authCtrl.logout = function(){
+            authCtrl.auth.$unauth();
+            authCtrl.authData = null;
+            authCtrl.message = null;
+        };
 
-            authCtrl.login = function () {
-                authCtrl.auth.$authWithPassword({
-                    email: authCtrl.email,
-                    password: authCtrl.password
-                })
-                    .then(function (authData) {
-                        authCtrl.email = '';
-                        authCtrl.password = '';
-                        authCtrl.message = 'You are logged in as ' + authData.uid;
-                    })
-                    .catch(function (error) {
-                        authCtrl.error = error;
-                    });
-            };
-
-            authCtrl.logout = function () {
-                authCtrl.auth.$unauth();
-            };
-
-            authCtrl.sendPwdReset = function () {
+        authCtrl.sendPwdReset = function(){
+            if ( authCtrl.email ) {
                 Auth.$resetPassword({
-                    email: authCtrl.email
-                }, function (error) {
-                    if (error === null) {
+                    email:authCtrl.email
+                }, function(error){
+                    if ( error === null ) {
                         authCtrl.message = 'Password reset e-mail was sent to: ' + authCtrl.email;
                     } else {
                         authCtrl.message = 'Error sending password reset email:' + error;
                     }
                 });
-            };
+            } else {
+                authCtrl.message = 'Please provide your e-mail address.'
+            }
+        };
 
-            // remove a user
-            authCtrl.removeUser = function () {
-                authCtrl.message = null;
-                authCtrl.error = null;
+        // remove a user
+        authCtrl.removeUser = function() {
+            authCtrl.message = null;
+            authCtrl.error = null;
 
-                Auth.$removeUser({
-                    email: authCtrl.email,
-                    password: authCtrl.password
-                }).then(function () {
-                    authCtrl.message = "User removed";
-                }).catch(function (error) {
-                    authCtrl.error = error;
-                });
-            };
-        }]);
-})();
+            Auth.$removeUser({
+                email: authCtrl.email,
+                password: authCtrl.password
+            }).then(function() {
+                authCtrl.message = "User removed";
+            }).catch(function(error) {
+                authCtrl.error = error;
+            });
+        };
+    }]);
--- app/scripts/auth/auth.service.js
+++ app/scripts/auth/auth.service.js
@@ -1,12 +1,9 @@
 /* global Firebase */
-(function () {
-    'use strict';
+'use strict';
 
-    angular.module('protrack')
-        .factory('Auth', ['$firebaseAuth', 'FirebaseUrl', function ($firebaseAuth, FirebaseUrl) {
-            var ref = new Firebase(FirebaseUrl.url);
-            var auth = $firebaseAuth(ref);
-
-            return auth;
-        }]);
-})();
+angular.module('protrack')
+    .factory('Auth', ['$firebaseAuth', 'FirebaseUrl', function($firebaseAuth, FirebaseUrl){
+        var ref = new Firebase(FirebaseUrl.url);
+        var auth = $firebaseAuth(ref);
+        return auth;
+    }]);
