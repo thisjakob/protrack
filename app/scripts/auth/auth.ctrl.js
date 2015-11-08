8e42bd1d7966b17260ec0f111106fec6399ffd20
thisjakob <this@mathias-jakobs-macbook.local>
2015-11-08 23:33:29 +0100

    nested auth.ctrl and auth.service in IIFE
    to get rid of some gulp error messages

--- app/scripts/auth/auth.ctrl.js
+++ app/scripts/auth/auth.ctrl.js
@@ -1,72 +1,74 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular.module('protrack')
-    .controller('AuthCtrl', ['Auth','$state', 'dataService', function(Auth, $state, dataService){
-        var authCtrl = this;
+    angular.module('protrack')
+        .controller('AuthCtrl', ['Auth', '$state', 'dataService', function (Auth, $state, dataService) {
+            var authCtrl = this;
 
-        authCtrl.authData = false;
-        authCtrl.message = null;
-        authCtrl.auth = Auth;
-
-        // any time auth status updates, add the user data to scope
-        authCtrl.auth.$onAuth(function(authData) {
-            if ( authData ) {
-                authCtrl.authData = authData;
-                dataService.getData('users/' + authData.uid + '/firstname').$loaded(function(data){
-                    authCtrl.firstname = data.$value;
-                });
-            }
-        });
-
-        authCtrl.login = function(){
-            authCtrl.auth.$authWithPassword({
-                email:authCtrl.email,
-                password:authCtrl.password
-            })
-            .then(function(authData){
-                authCtrl.email = '';
-                authCtrl.password = '';
-                authCtrl.message = 'You are logged in as ' + authData.uid;
-            })
-            .catch(function(error){
-                authCtrl.error = error;
-            });
-        };
-
-        authCtrl.logout = function(){
-            authCtrl.auth.$unauth();
-            authCtrl.authData = null;
+            authCtrl.authData = false;
             authCtrl.message = null;
-        };
+            authCtrl.auth = Auth;
 
-        authCtrl.sendPwdReset = function(){
-            if ( authCtrl.email ) {
-                Auth.$resetPassword({
-                    email:authCtrl.email
-                }, function(error){
-                    if ( error === null ) {
-                        authCtrl.message = 'Password reset e-mail was sent to: ' + authCtrl.email;
-                    } else {
-                        authCtrl.message = 'Error sending password reset email:' + error;
-                    }
-                });
-            } else {
-                authCtrl.message = 'Please provide your e-mail address.'
-            }
-        };
-
-        // remove a user
-        authCtrl.removeUser = function() {
-            authCtrl.message = null;
-            authCtrl.error = null;
-
-            Auth.$removeUser({
-                email: authCtrl.email,
-                password: authCtrl.password
-            }).then(function() {
-                authCtrl.message = "User removed";
-            }).catch(function(error) {
-                authCtrl.error = error;
+            // any time auth status updates, add the user data to scope
+            authCtrl.auth.$onAuth(function (authData) {
+                if (authData) {
+                    authCtrl.authData = authData;
+                    dataService.getData('users/' + authData.uid + '/firstname').$loaded(function (data) {
+                        authCtrl.firstname = data.$value;
+                    });
+                }
             });
-        };
-    }]);
+
+            authCtrl.login = function () {
+                authCtrl.auth.$authWithPassword({
+                    email: authCtrl.email,
+                    password: authCtrl.password
+                })
+                    .then(function (authData) {
+                        authCtrl.email = '';
+                        authCtrl.password = '';
+                        authCtrl.message = 'You are logged in as ' + authData.uid;
+                    })
+                    .catch(function (error) {
+                        authCtrl.error = error;
+                    });
+            };
+
+            authCtrl.logout = function () {
+                authCtrl.auth.$unauth();
+                authCtrl.authData = null;
+                authCtrl.message = null;
+            };
+
+            authCtrl.sendPwdReset = function () {
+                if (authCtrl.email) {
+                    Auth.$resetPassword({
+                        email: authCtrl.email
+                    }, function (error) {
+                        if (error === null) {
+                            authCtrl.message = 'Password reset e-mail was sent to: ' + authCtrl.email;
+                        } else {
+                            authCtrl.message = 'Error sending password reset email:' + error;
+                        }
+                    });
+                } else {
+                    authCtrl.message = 'Please provide your e-mail address.';
+                }
+            };
+
+            // remove a user
+            authCtrl.removeUser = function () {
+                authCtrl.message = null;
+                authCtrl.error = null;
+
+                Auth.$removeUser({
+                    email: authCtrl.email,
+                    password: authCtrl.password
+                }).then(function () {
+                    authCtrl.message = "User removed";
+                }).catch(function (error) {
+                    authCtrl.error = error;
+                });
+            };
+        }]);
+})();
--- app/scripts/auth/auth.service.js
+++ app/scripts/auth/auth.service.js
@@ -1,9 +1,13 @@
 /* global Firebase */
-'use strict';
 
-angular.module('protrack')
-    .factory('Auth', ['$firebaseAuth', 'FirebaseUrl', function($firebaseAuth, FirebaseUrl){
-        var ref = new Firebase(FirebaseUrl.url);
-        var auth = $firebaseAuth(ref);
-        return auth;
-    }]);
+(function () {
+    'use strict';
+
+
+    angular.module('protrack')
+        .factory('Auth', ['$firebaseAuth', 'FirebaseUrl', function ($firebaseAuth, FirebaseUrl) {
+            var ref = new Firebase(FirebaseUrl.url);
+            var auth = $firebaseAuth(ref);
+            return auth;
+        }]);
+})();
