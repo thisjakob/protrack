67bb66db03f34272c913c97d0bd68a14c96a99bb
thisjakob <this@mathias-jakobs-macbook.local>
2015-11-18 17:10:10 +0100

    Simplyfied diffTime() in tracks.service.js

--- app/scripts/tracks/tracks.service.js
+++ app/scripts/tracks/tracks.service.js
@@ -6,15 +6,16 @@
     angular.module('protrack').factory('calcTime', [function () {
 
         var diffTime = function (startTime, endTime) {
-            var difftime = '00:00';
-            if (moment(startTime, "DD.MM.YYYY HH:mm").isValid() && moment(endTime, "DD.MM.YYYY HH:mm").isValid()) {
-                // difftime = endtime - starttime
-                var diff = moment.utc(moment(endTime,"DD.MM.YYYY HH:mm").diff(moment(startTime,"DD.MM.YYYY HH:mm"))).format("HH:mm");
-                difftime = diff;
+            var diff = '00:00';
+            var start = moment(startTime, 'DD.MM.YYYY HH:mm');
+            var end = moment(endTime, 'DD.MM.YYYY HH:mm');
+
+            if (start.isValid() && end.isValid()) {
+                diff = moment.utc(end.diff(start)).format("HH:mm");
             } else {
                 console.log('endtime or starttime is not valid!');
             }
-            return difftime;
+            return diff;
         };
 
         var addDiffTime = function (time, diffTime) {
