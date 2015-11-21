1cefa8c21fc04138072c539309394e95e23263cd
thisjakob <this@mathias-jakobs-macbook.local>
2015-11-21 13:45:55 +0100

    Added filters for tracks
    - „durationsec“: shows duration in the format „HH:mm:ss“
    - „durationmin“: shows duration in format „HH:mm“
    
    Also optimized filter code regarding DRY

--- app/scripts/tracks/tracks.filter.js
+++ app/scripts/tracks/tracks.filter.js
@@ -4,11 +4,35 @@
     angular.module('protrack')
         .filter('datetimenosecond', function () {
             return function (item) {
-                if (moment(item, 'DD.MM.YYYY HH:mm:ss').isValid()) {
-                    return moment(item, 'DD.MM.YYYY HH:mm:ss').format('DD.MM.YYYY HH:mm');
+                var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
+                if ( date.isValid() ) {
+                    return date.format('DD.MM.YYYY HH:mm');
+                } else {
+                    return 'time is not valid';
+                }
+            };
+        })
+
+        .filter('durationsec', function () {
+            return function (item) {
+                var duration = moment(item, 'HH:mm:ss');
+                if (duration.isValid()) {
+                    return duration.format('HH:mm:ss');
+                } else {
+                    return 'time is not valid';
+                }
+            };
+        })
+
+        .filter('durationmin', function () {
+            return function (item) {
+                var duration = moment(item, 'HH:mm:ss');
+                if (duration.isValid()) {
+                    return duration.format('HH:mm');
                 } else {
                     return 'time is not valid';
                 }
             };
         });
+
 })();
