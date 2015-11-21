eed94258e81ce406b3b93eb234f7482381bce4f2
thisjakob <this@mathias-jakobs-macbook.local>
2015-11-21 15:05:57 +0100

    Added seconds to difftime

--- app/scripts/tracks/tracks.service.js
+++ app/scripts/tracks/tracks.service.js
@@ -6,12 +6,12 @@
     angular.module('protrack').factory('calcTime', [function () {
 
         var diffTime = function (startTime, endTime) {
-            var diff = '00:00';
-            var start = moment(startTime, 'DD.MM.YYYY HH:mm');
-            var end = moment(endTime, 'DD.MM.YYYY HH:mm');
+            var diff = '00:00:00';
+            var start = moment(startTime, 'DD.MM.YYYY HH:mm:ss');
+            var end = moment(endTime, 'DD.MM.YYYY HH:mm:ss');
 
             if ( start.isValid() && end.isValid() ) {
-                diff = moment.utc(end.diff(start)).format("HH:mm");
+                diff = moment.utc(end.diff(start)).format("HH:mm:ss");
             } else {
                 console.log('endtime or starttime is not valid!');
             }
