47b130ef466264274f6f71bda6a3f2554308ee34
gip <pascal@GipAir.local>
2015-11-24 21:42:42 +0100

    fix diffTime calculate over 1 day

--- app/scripts/tracks/tracks.service.js
+++ app/scripts/tracks/tracks.service.js
@@ -6,17 +6,19 @@
     angular.module('protrack').factory('calcTime', [function () {
 
         var diffTime = function (startTime, endTime) {
-            var diff = '00:00:00';
+            var hours = '00:00:00';
+            var diff = '';
             var start = moment(startTime, 'DD.MM.YYYY HH:mm:ss');
             var end = moment(endTime, 'DD.MM.YYYY HH:mm:ss');
 
             if ( start.isValid() && end.isValid() ) {
-                diff = moment.utc(end.diff(start)).format("HH:mm:ss");
+                diff = moment.utc(end.diff(start)).format("DDHH:mm:ss");
+                hours = ((Number(diff.substr(0, 2)) - 1) * 24 + Number(diff.substr(2, 2))).toString() + diff.substr(4);
             } else {
                 console.log('endtime or starttime is not valid!');
             }
 
-            return diff;
+            return hours;
         };
 
         var addDiffTime = function (time, diffTime) {
