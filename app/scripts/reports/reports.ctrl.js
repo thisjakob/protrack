385579849917019492877b248749ec547a766670
thisjakob <this@mathias-jakobs-macbook.local>
2015-11-25 23:45:40 +0100

    merged changes by gip with mine

--- app/scripts/reports/reports.ctrl.js
+++ app/scripts/reports/reports.ctrl.js
@@ -1,11 +1,10 @@
 (function () {
     'use strict';
-// TODO export to csv
-// TODO total of Duration
+    // TODO export to csv
     // TODO Filter for Project und Tag
     // TODO search Description
     angular.module('protrack')
-        .controller('ReportsCtrl', ['dataService', 'authData', 'showData', function (dataService, authData, showData) {
+        .controller('ReportsCtrl', ['dataService', 'authData', 'showData', 'reportUtilities', function (dataService, authData, showData, reportUtilities) {
             var reportsCtrl = this;
             var path = 'users/' + authData.uid + '/';
 
@@ -46,13 +45,51 @@
             reportsCtrl.saveDate = function () {
                 var date = reportsCtrl.dateFrom.toString();
                 dataService.setData(path + 'settings/daterange/from', date);
-/*                if (!isFinite(reportsCtrl.dateTo)) {
-                    reportsCtrl.dateTo = new Date();
-                }*/
-                reportsCtrl.dateTo.setHours(reportsCtrl.dateTo.getHours() + 23);
-                reportsCtrl.dateTo.setMinutes(reportsCtrl.dateTo.getMinutes() + 59);
+                if (reportsCtrl.dateTo.getHours() === 0) {
+                    reportsCtrl.dateTo.setHours(reportsCtrl.dateTo.getHours() + 23);
+                    reportsCtrl.dateTo.setMinutes(reportsCtrl.dateTo.getMinutes() + 59);
+                }
                 date = reportsCtrl.dateTo.toString();
                 dataService.setData(path + 'settings/daterange/to', date);
+            };
+
+            reportsCtrl.sumDur = function (tracks) {
+                var sum = reportUtilities.sumDuration(tracks);
+                return sum.substr(0, sum.length - 3);
+            };
+
+            /**
+             * push each track as array in a array
+             * @param tracks
+             * @returns {Array} array of array of track-data
+             */
+            reportsCtrl.writeCsv = function (tracks) {
+                var csv = [];
+                angular.forEach (tracks, function (track) {
+                    var item = [];
+                    item.push("'" + track.desc.replace(/'/,"\'") + "'");
+                    item.push("'" + showData.showDataName(reportsCtrl.projectsArray, track.project) + "'");
+                    var tags = '';
+                    angular.forEach(track.tags, function(tag) {
+                        tags = tags + showData.showDataName(reportsCtrl.tagsArray, tag);
+                    });
+                    item.push("'" + tags.replace(/'/,"\'") + "'");
+                    item.push("'" + track.starttime + "'");
+                    item.push("'" + track.endtime + "'");
+                    item.push("'" + track.difftime + "'");
+                    csv.push(item);
+                });
+                return csv;
+            };
+
+            /**
+             * build filename with actual datetime
+             * @returns {string}
+             */
+            reportsCtrl.getFilename = function() {
+                var from = reportsCtrl.dateFrom.getFullYear().toString() + '_'+ reportsCtrl.dateFrom.getMonth() + '_'+ reportsCtrl.dateFrom.getDate();
+                var to = reportsCtrl.dateTo.getFullYear().toString() + '_'+ reportsCtrl.dateTo.getMonth() + '_'+ reportsCtrl.dateTo.getDate();
+                return 'protrack-' + from + '-' + to + '.csv';
             };
         }]);
 })();
