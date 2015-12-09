010ac603ee167bfc463928909ba430496bc8fcd8
thisjakob <this@mathias-jakobs-macbook.local>
2015-12-09 23:36:27 +0100

    fixed indentation and added filter timeonlymin

--- app/scripts/tracks/tracks.filter.js
+++ app/scripts/tracks/tracks.filter.js
@@ -5,71 +5,85 @@
     /**
      * format datetime without second
      */
-        .filter('datetimenosecond', function () {
-            return function (item) {
-                var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
-                if ( date.isValid() ) {
-                    return date.format('DD.MM.YYYY HH:mm');
-                } else {
-                    return 'time is not valid';
-                }
-            };
-        })
+    .filter('datetimenosecond', function () {
+        return function (item) {
+            var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
+            if ( date.isValid() ) {
+                return date.format('DD.MM.YYYY HH:mm');
+            } else {
+                return 'time is not valid';
+            }
+        };
+    })
 
     /**
-     * format duration with seconds
+     * format duration HH:mm:ss
      */
-        .filter('durationsec', function () {
-            return function (item) {
-                var duration = moment(item, 'HH:mm:ss');
-                if (duration.isValid()) {
-                    return duration.format('HH:mm:ss');
-                } else {
-                    return 'time is not valid';
-                }
-            };
-        })
+    .filter('durationsec', function () {
+        return function (item) {
+            var duration = moment(item, 'HH:mm:ss');
+            if (duration.isValid()) {
+                return duration.format('HH:mm:ss');
+            } else {
+                return 'time is not valid';
+            }
+        };
+    })
 
     /**
-     * format duration with seconds
+     * format duration HH:mm
      */
-        .filter('durationmin', function () {
-            return function (item) {
-                var duration = moment(item, 'HH:mm:ss');
-                if (duration.isValid()) {
-                    return duration.format('HH:mm');
-                } else {
-                    return 'time is not valid';
-                }
-            };
-        })
+    .filter('durationmin', function () {
+        return function (item) {
+            var duration = moment(item, 'HH:mm:ss');
+            if (duration.isValid()) {
+                return duration.format('HH:mm');
+            } else {
+                return 'time is not valid';
+            }
+        };
+    })
 
     /**
      * format datetime to date
      */
-        .filter('dateonly', function () {
-            return function (item) {
-                var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
-                if ( date.isValid() ) {
-                    return date.format('DD.MM.YYYY');
-                } else {
-                    return 'Date is not valid';
-                }
-            };
-        })
+    .filter('dateonly', function () {
+        return function (item) {
+            var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
+            if ( date.isValid() ) {
+                return date.format('DD.MM.YYYY');
+            } else {
+                return 'Date is not valid';
+            }
+        };
+    })
 
     /**
-     * format datetime to time
+     * format datetime to time HH:mm:ss
      */
-        .filter('timeonly', function () {
-            return function (item) {
-                var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
-                if ( date.isValid() ) {
-                    return date.format('HH:mm:ss');
-                } else {
-                    return 'Time is not valid';
-                }
-            };
-        });
+    .filter('timeonly', function () {
+        return function (item) {
+            var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
+            if ( date.isValid() ) {
+                return date.format('HH:mm:ss');
+            } else {
+                return 'Time is not valid';
+            }
+        };
+    })
+
+    /**
+     * format datetime to time HH:mm
+     */
+    .filter('timeonlymin', function () {
+        return function (item) {
+            var date = moment(item, 'DD.MM.YYYY HH:mm:ss');
+            if ( date.isValid() ) {
+                return date.format('HH:mm');
+            } else {
+                return 'Time is not valid';
+            }
+        };
+    });
 
 })();
